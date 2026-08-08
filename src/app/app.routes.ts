7556b5c6d4228111c94d209859b1b89cba586f59
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-08-08 18:01:24 +0200

    Registers mask

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/bbox/bbox').then((component) => component.Bbox),
     title: 'BBox · GeoTools'
   },
+  {
+    path: 'mask',
+    loadComponent: () => import('./pages/mask/mask').then((component) => component.Mask),
+    title: 'GeoJSON Mask · GeoTools'
+  },
   { path: '', pathMatch: 'full', redirectTo: 'bbox' },
   { path: '**', redirectTo: 'bbox' }
 ];
